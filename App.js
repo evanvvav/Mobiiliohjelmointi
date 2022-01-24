9650c96cabf7aca8c2171f21b4ed0665c4175d57
evanvvav <ivankolesov.fi@gmail.com>
2022-01-24 17:01:13 +0200

    0.1

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput, Text, Button, View, Alert } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [number1, onChangeNumber1] = React.useState(null);
+  const [number2, onChangeNumber2] = React.useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+
+      <Text style={styles.Text}>
+        Result: </Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber1}
+        value={number1}
+        placeholder="number 1"
+        keyboardType="numeric"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber2}
+        value={number2}
+        placeholder="number 2"
+        keyboardType="numeric"
+      />
+  
+      <View style={styles.fixToText}>
+        <Button
+          title="+ Function"
+          onPress={() => Alert.alert("Answer is " + (number1 + number2))}
+        />
+        <Button
+          title="- Function"
+          onPress={() => Alert.alert("Answer is " + (number1 - number2))}
+        />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10
+    ,
+  },
+  Text: {
+    padding: 10,
+    fontSize: 20,
+
   },
 });
+
+export default App;
