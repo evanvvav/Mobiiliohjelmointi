7cb09654375a59f6a1ddafd6886b4fa36fcdc215
evanvvav <ivankolesov.fi@gmail.com>
2022-02-01 18:47:31 +0200

    Tehtävä tehty

--- App.js
+++ App.js
@@ -1,19 +1,71 @@
-import { StyleSheet } from "react-native";
-import { NavigationContainer } from'@react-navigation/native';
-import { createNativeStackNavigator } from'@react-navigation/native-stack';
-import HomeScreen from "./components/HomeScreen";
-import HistoryScreen from "./components/HistoryScreen";
+import { useEffect, useState } from "react";
+import { StyleSheet, SafeAreaView, Text, View, Button, TextInput, FlatList, Alert, Image} from "react-native";
+
 
 export default function App() {
 
-  const Stack = createNativeStackNavigator();
+  const [keyword, setKeyword] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  const getRepositories = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`)
+    .then(response => response.json())
+    .then(data => setRepositories(data.meals)) 
+    .catch(error => {
+        Alert.alert('Error', error);
+
+    });
+  }
 
    return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="History" component={HistoryScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.main}>
+        <TextInput style={styles.input} onChangeText={ text => setKeyword(text) }/>
+        <Button title="Find" onPress = {getRepositories}/>
+
+      <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) =>
+          <View>
+            <Text
+              style={{fontSize:18, fontWeight: "bold"}}>{item.strMeal}
+            </Text>
+            <Image source={{uri: item.strMealThumb}}
+             style={{width:200, height: 200}} />
+            
+      
+            
+          </View>}
+          data={repositories}/>
+    </SafeAreaView> 
   );
 }
+
+const styles = StyleSheet.create({
+  main: {
+    paddingTop: 60,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'silver',
+  },
+  text: {
+    fontSize: 18,
+  },
+  input: {
+    borderWidth: 1,
+    width: 160,
+    height: 30,
+    margin: 1,
+    backgroundColor: 'white',
+    
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '40%',
+    margin: 4,
+  },
+  button: {
+    width: 57,
+  },
+});
